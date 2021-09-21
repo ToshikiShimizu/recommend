78755b74fad40957effdf59906da7603c1854960
清水俊樹 <tamreff3290@gmail.com>
2021-09-21 21:54:39 +0900

    ユークリッド距離による類似度計算

--- programming_collective_intelligence/src/recommendations.py
+++ programming_collective_intelligence/src/recommendations.py
@@ -14,3 +14,18 @@ def distance_euclidean(v1: np.ndarray, v2: np.ndarray) -> float:
     # np.linalg.norm(v1-v2)としてもよい
     dist: float = np.sqrt(np.sum(np.square(v1 - v2)))
     return dist
+
+
+def sim_distance_euclidean(v1: np.ndarray, v2: np.ndarray) -> float:
+    """ユークリッド距離を利用した類似度を計算する
+
+    Args:
+        arg1 (np.ndarray): ベクトル
+        arg2 (np.ndarray): ベクトル
+
+    Returns:
+        float: 類似度
+    """
+    # np.linalg.norm(v1-v2)としてもよい
+    sim: float = 1 / (1 + distance_euclidean(v1, v2))
+    return sim
--- programming_collective_intelligence/tests/test_recommendations.py
+++ programming_collective_intelligence/tests/test_recommendations.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from src.recommendations import distance_euclidean
+from src.recommendations import distance_euclidean, sim_distance_euclidean
 
 
 @pytest.mark.parametrize('v1,v2,expected', [
@@ -11,3 +11,13 @@ from src.recommendations import distance_euclidean
 ])
 def test_distance_euclidean(v1, v2, expected):
     assert distance_euclidean(v1, v2) == expected
+
+
+@pytest.mark.parametrize('v1,v2,expected', [
+    (np.array([1, 2]), np.array([1, 2]), 1),
+    (np.array([0, 0]), np.array([1, 1]), 1/(1+np.sqrt(2))),
+    (np.array([0, 0]), np.array([3, 4]), 1/6),
+    (np.array([1]), np.array([2]), 1/2),
+])
+def test_sim_distance_euclidean(v1, v2, expected):
+    assert sim_distance_euclidean(v1, v2) == expected
