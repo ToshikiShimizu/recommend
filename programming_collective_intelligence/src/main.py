b872e460d6f60c487e697ea9e2bc276a212c76a6
清水俊樹 <tamreff3290@gmail.com>
2021-09-27 21:49:45 +0900

    ユーザ間類似度の計算サンプル

--- programming_collective_intelligence/src/main.py
+++ programming_collective_intelligence/src/main.py
@@ -1,10 +1,11 @@
-from recommendations import load_critics
+from recommendations import load_critics, calc_similarity
 
 
 def main() -> None:
     critics_matrix, user_dic, item_dic = load_critics()
     print(critics_matrix, user_dic, item_dic)
-
+    sim = calc_similarity(critics_matrix[0], critics_matrix[1])
+    print(sim)
 
 if __name__ == "__main__":
     main()
