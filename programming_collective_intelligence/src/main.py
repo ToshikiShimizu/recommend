d628a79a3a90b8e6aeb060396fdef0bee22880df
清水俊樹 <tamreff3290@gmail.com>
2021-09-29 21:08:49 +0900

    top_matchesのサンプルをmainに実装

--- programming_collective_intelligence/src/main.py
+++ programming_collective_intelligence/src/main.py
@@ -1,6 +1,6 @@
 from recommendations import calc_similarity
 from critic import Critic
-
+import numpy as np
 
 def main() -> None:
     critic = Critic()
@@ -8,6 +8,19 @@ def main() -> None:
     sim = calc_similarity(critic.matrix[0], critic.matrix[1])
     print(sim)
 
+    # top_matches
+    user_list = list(critic.user_dic.keys())
+    user_name = user_list[0]
+    print(user_name)
+    sims = []
+    for k, v in critic.user_dic.items():
+        v1 = critic.matrix[critic.user_dic[user_name]]
+        v2 = critic.matrix[v]
+        sim = calc_similarity(v1, v2)
+        sims.append(sim)
+    idx = np.argsort(sims)[::-1]
+    for i in idx:
+        print(user_list[i], sims[i])
 
 if __name__ == "__main__":
     main()
