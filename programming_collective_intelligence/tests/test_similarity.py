547be4d3e9f27668d17912b43a3b041680fdda81
清水俊樹 <tamreff3290@gmail.com>
2021-10-06 08:54:14 +0900

    pytestにidsを追加

--- programming_collective_intelligence/tests/test_similarity.py
+++ programming_collective_intelligence/tests/test_similarity.py
@@ -8,7 +8,7 @@ import similarity
     (np.array([0, 0]), np.array([1, 1]), np.sqrt(2)),
     (np.array([0, 0]), np.array([3, 4]), 5),
     (np.array([1]), np.array([2]), 1),
-])
+], ids=['same_point', 'root', 'integer', '1-dimentional_vector'])
 def test_calc_euclidean_distance(v1, v2, expected):
     assert similarity.calc_euclidean_distance(v1, v2) == expected
 
@@ -18,7 +18,7 @@ def test_calc_euclidean_distance(v1, v2, expected):
     (np.array([0, 0]), np.array([1, 1]), 1/(1+np.sqrt(2))),
     (np.array([0, 0]), np.array([3, 4]), 1/6),
     (np.array([1]), np.array([2]), 1/2),
-])
+], ids=['same_point', 'root', 'integer', '1-dimentional_vector'])
 def test_calc_euclidean_similarity(v1, v2, expected):
     assert similarity.calc_euclidean_similarity(v1, v2) == expected
 
@@ -28,7 +28,7 @@ def test_calc_euclidean_similarity(v1, v2, expected):
     (np.array([1, 2]), np.array([1, 0]), 1),
     (np.array([0, 0]), np.array([3, 4]), 0),
     (np.array([1]), np.array([2]), 1/2),
-])
+], ids=['same_point', 'one_missing_value', 'all_missing_value', '1-dimentional_vector'])
 def test_calc_similarity_with_missing_value(v1, v2, expected):
     assert similarity.calc_similarity_with_missing_value(
         v1, v2) == expected
@@ -40,7 +40,7 @@ def test_calc_similarity_with_missing_value(v1, v2, expected):
     (np.array([1, 2]), np.array([3, 3]), 0),
     (np.array([1, 2, 3]), np.array([1, 3, 2]), 0.5),
     (np.array([1]), np.array([2]), 0),
-])
+], ids=['1', '-1', 'constant', 'float', '1-dimentional_vector'])
 def test_calc_pearson_correlation_coefficient(v1, v2, expected):
     assert similarity.calc_pearson_correlation_coefficient(
         v1, v2) == expected
@@ -48,7 +48,7 @@ def test_calc_pearson_correlation_coefficient(v1, v2, expected):
 
 @pytest.mark.parametrize('v1,v2', [
     (np.array([1, 2, 3]), np.array([1, 3, 2])),
-])
+], ids=['float'])
 def test_calc_similarity(v1, v2):
     assert similarity.calc_similarity(
         v1, v2, metric='euclidean') == similarity.calc_euclidean_similarity(v1, v2)
